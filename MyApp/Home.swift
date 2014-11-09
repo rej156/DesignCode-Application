9ebb7a6bd923e13086beb8c01faa8243d9da234e
Jesse James <jesse.james@albionlondon.com>
2014-11-09 16:33:16 +0000

    first commit

---
+++ MyApp/Home.swift
@@ -0,0 +1,30 @@
+//
+//  Home.swift
+//  MyApp
+//
+//  Created by Jesse James on 09/11/2014.
+//  Copyright (c) 2014 Jesse James. All rights reserved.
+//
+
+import UIKit
+
+class Home: UIViewController {
+
+    @IBOutlet weak var dialogView: UIView!
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // begin
+        let scale = CGAffineTransformMakeScale(0.5, 0.5)
+        let translate = CGAffineTransformMakeTranslation(0, -200)
+        dialogView.transform = CGAffineTransformConcat(scale, translate)
+        
+        UIView.animateWithDuration(5, animations: {
+
+            // end
+            self.dialogView.transform = CGAffineTransformIdentity
+        })
+    }
+
+}
